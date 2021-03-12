a4fa83806b8e06acf784fbd34a6b6305c6835ff9
Mr.K <48730446+MrK-07@users.noreply.github.com>
2021-03-12 16:06:19 -0300

    Começando

---
+++ resources/views/paginas/chamadaSenha.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!-- CSS da Página -->
+    <link href="/css/chamadaSenha.css" rel="stylesheet"/>
+    <title>Tela de Chamada</title>
+</head>
+<body>
+    <!-- Conteúdo da Página -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm" id="divSenha">
+                <span id="lblSenha">SENHA:</span>
+                <p>63<p>
+            </div>
+            <div class="col-sm" id="divGuiche">
+                <span id="guiche">GUICHÊ:</span>
+            </div>
+            
+        </div>
+    </div>
+
+
+
+    <!-- Pacotes JS Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+</body>
+</html>
